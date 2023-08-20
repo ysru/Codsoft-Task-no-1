00dc352884eb9b76ed078d9200c6c629a1ae4c48
Yogendra Katuwal <120618811+ysru@users.noreply.github.com>
2023-08-20 12:15:43 +0530

    Update script.js
    ...

--- Landing page/script.js
+++ Landing page/script.js
@@ -1,6 +1,1 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
+//java script fiel
